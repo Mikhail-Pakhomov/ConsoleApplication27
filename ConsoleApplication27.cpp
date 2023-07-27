c8419e5007e2f2a579470556341b044fb3ad4c98
Mikhail-Pakhomov <121101457+Mikhail-Pakhomov@users.noreply.github.com>
2023-07-27 16:54:01 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication27.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+
+class Tree {
+public:
+    virtual void draw() const = 0;
+};
+
+class TreePlus : public Tree {
+public:
+    void draw() const override {
+        std::cout << "/ \\\n"
+            "// \\\\\n";
+    }
+};
+
+class TreeAsterisk : public Tree {
+public:
+    void draw() const override {
+        std::cout << "/ \\\n"
+            "/ ** \\\n";
+    }
+};
+
+class TreeSlash : public Tree {
+public:
+    void draw() const override {
+        std::cout << "/ \\\n"
+            "/ ++ \\\\\n";
+    }
+};
+
+int main() {
+    TreePlus plusTree;
+    TreeAsterisk asteriskTree;
+    TreeSlash slashTree;
+
+    Tree* trees[3] = { &plusTree, &asteriskTree, &slashTree };
+
+    for (int i = 0; i < 3; i++) {
+        trees[i]->draw();
+        std::cout << "Малюнок " << i + 1 << ":\n";
+    }
+
+    return 0;
+}
